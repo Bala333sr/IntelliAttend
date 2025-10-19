82d7ad29935bc8ff856457d4829e64538e648dd9
Your Name <you@example.com>
2025-10-19 13:54:36 +0530

    Remove deleted files from tracking

--- mobile/app/app/src/main/java/com/intelliattend/student/data/model/BeaconInfo.kt
+++
@@ -1,9 +0,0 @@
-package com.intelliattend.student.data.model
-
-data class BeaconInfo(
-    val classId: Int,
-    val sessionToken: Int,
-    val facultyId: Int,
-    val flags: Byte,
-    val signature: ByteArray
-)
